5a5a23def32006b4e1bb1234625f4eebf412de34
luanjosecar <luan.jose.c@gmail.com>
2022-08-21 21:38:17 -0300

    ajustes readings

--- routes/aux/readings.py
+++ routes/aux/readings.py
@@ -1,7 +1,8 @@
 from ..database.postgres_reader import *
 from datetime import datetime
 from .responses import *
-
+import os
+from dotenv import load_dotenv
 
 def get_readings(user_id:str):
     query = f"select read_id, read_imp, read_date, read_status from readings where user_id = '{user_id}'"
@@ -13,10 +14,18 @@ def get_readings(user_id:str):
     return base_list
 
 
-
 # Definir banco de dados no qual ela seram colocadas
-def upload_img(image):
-    pass
+def upload_img(image_data, reading_id, index, img_type):
+    load_dotenv()
+    bucket = os.getenv('BUCKETNAME')
+    project_id = os,getenv('PROJECTID')
+    image_path = img_type + '/' + reading_id + str(index) + ".jpg"
+    
+    client = storage.Client(project_id)
+    bucket = client.get_bucket(bucket)
+    blob = bucket.blob(image_path)
+    blob.upload_from_string(image_data)
+    return image_path
 
 # Metodo para inserção de leituras dentro do sistema
 def insert_reading(reading_id:str, user_id:str, cnn_id:str):
@@ -25,17 +34,16 @@ def insert_reading(reading_id:str, user_id:str, cnn_id:str):
         VALUES ('{reading_id}', '{user_id}', '{cnn_id}', '{datetime.now()}', False)
     """
     send_request(query)
-    # try:
-    #     send_request(query)
-    #     return sucess_resp("reading added")
-    # except:
-    #     return error_resp("Unable to regist reading")
+
     
 # insere imagens para validação
-def insert_imgs(reading_id: str, image_id:str):
+def insert_imgs(reading_id: str, image_dir:str):
+    load_dotenv()
+    bucket = os.getenv('BUCKETNAME')
+    image_path = f"https://storage.googleapis.com/{bucket}/{image_dir}"
     query = f"""
-        INSERT INTO images_reading (imagem,reading_id)
-        VALUES ('{image_id}', '{reading_id}')
+        INSERT INTO reading_imgs (read_id,img_location)
+        VALUES ('{reading_id}', '{image_path}')
     """
     send_request(query)
 
